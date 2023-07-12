7f323a9000f4967fef82a8a3e1f562662d4df86d
Sergey Kemenov <skemenov@icloud.com>
2023-07-13 00:18:15 +0300

    Added outlets & config() into `ImagesListCell` class

--- ImageFeed-ios/ImageList/ImageListCell.swift
+++ ImageFeed-ios/ImageList/ImageListCell.swift
@@ -5,4 +5,22 @@
 //  Created by Sergey Kemenov on 12.07.2023.
 //
 
-import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+  static let reuseIdentifier = "ImagesListCell"
+
+  @IBOutlet var imageCell: UIImageView!
+  @IBOutlet var dateLabel: UILabel!
+  @IBOutlet var likeButton: UIButton!
+}
+
+extension ImagesListCell {
+  func config(image: UIImage?, date: String, isLiked: Bool) {
+    let likedImage = isLiked ? UIImage(named: "likeActive") : UIImage(named: "likeNoActive")
+
+    imageCell.image = image
+    dateLabel.text = date
+    likeButton.setImage(likedImage, for: .normal)
+  }
+}
