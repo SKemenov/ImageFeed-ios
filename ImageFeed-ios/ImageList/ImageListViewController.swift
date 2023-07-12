ac55e9bd9b6a0b81bd8ab5fd6893fb401e6c70c9
Sergey Kemenov <skemenov@icloud.com>
2023-07-13 00:20:56 +0300

    Conformed ImagesListViewController into UITableViewDelegate & UITableViewDataSource protocols

--- ImageFeed-ios/ImageList/ImageListViewController.swift
+++ ImageFeed-ios/ImageList/ImageListViewController.swift
@@ -33,6 +33,55 @@ class ImagesListViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    tableView.dataSource = self
+    tableView.delegate = self
+    tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 0, right: 0)
+    //    print(photosName)
+  }
+  // MARK: - Actions
+
+  // MARK: - Methods
+
+  // MARK: - Private methods
+
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    photosName.count
+  }
+
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    guard let image = UIImage(named: photosName[indexPath.row]) else {
+      return 0
+    }
+    let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+    let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+    let imageWidth = image.size.width
+    let scale = imageViewWidth / imageWidth
+    let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+    return cellHeight
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(
+      withIdentifier: ImagesListCell.reuseIdentifier,
+      for: indexPath
+    ) as? ImagesListCell else {
+      return UITableViewCell()
+    }
+
+    let image = UIImage(named: photosName[indexPath.row])
+    let date = dateFormatter.string(from: Date())
+    let isLiked = indexPath.row % 2 != 0
+    cell.config(image: image, date: date, isLiked: isLiked)
+
+    return cell
   }
 }
