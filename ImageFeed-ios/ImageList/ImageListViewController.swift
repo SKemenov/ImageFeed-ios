9cd097273b8fa19d03725da7e2b3c4c385381f13
Sergey Kemenov <skemenov@icloud.com>
2023-07-13 00:19:36 +0300

    Added photosName array, date formatter & made status bar white

--- ImageFeed-ios/ImageList/ImageListViewController.swift
+++ ImageFeed-ios/ImageList/ImageListViewController.swift
@@ -7,7 +7,30 @@
 
 import UIKit
 
-class ImageListViewController: UIViewController {
+class ImagesListViewController: UIViewController {
+  // MARK: - Outlets
+
+  @IBOutlet private var tableView: UITableView!
+
+  // MARK: - Properties
+
+  private let photosName: [String] = Array(0..<20).map { "\($0)" }
+
+  private lazy var dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .long
+    formatter.timeStyle = .none
+    return formatter
+  }()
+
+  // MARK: - Public properties
+
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+
+  // MARK: - Lifecycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
