b5a2f45fa95b1c14fa497af540bc5bb3cce817a8
Sergey Kemenov <skemenov@icloud.com>
2023-07-18 11:56:36 +0300

    Added ProfileViewController, link outlets & an action, mock data

---
+++ ImageFeed-ios/ImageList/ProfileViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed-ios
+//
+//  Created by Sergey Kemenov on 18.07.2023.
+//
+
+import UIKit
+
+final  class ProfileViewController: UIViewController {
+  // MARK: - IBOutlets
+  @IBOutlet private weak var profilePhotoImage: UIImageView!
+  @IBOutlet private weak var profileFullNameLabel: UILabel!
+  @IBOutlet private weak var profileLoginNameLabel: UILabel!
+  @IBOutlet private weak var profileBioLabel: UILabel!
+  @IBOutlet private weak var exitButton: UIButton!
+
+  // MARK: - Public properties
+
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+
+  // MARK: - Lifecyrcle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    profilePhotoImage?.image = UIImage(named: "Photo")
+    profileFullNameLabel.text = "Константин Константинопольский"
+    profileLoginNameLabel.text = "@konstantin_kon"
+    profileBioLabel.text = "Hello, swift!"
+  }
+
+  // MARK: - Actions
+  @IBAction func exitButtonClicked() {
+  }
+}
