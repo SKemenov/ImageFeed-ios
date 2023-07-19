e80767421ca519f792f9cb91d62c6bb094ab2ea4
Sergey Kemenov <skemenov@icloud.com>
2023-07-19 18:36:44 +0300

    Added `SingleImageViewController` class

---
+++ ImageFeed-ios/ImageList/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed-ios
+//
+//  Created by Sergey Kemenov on 19.07.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+  // MARK: - Outlets
+
+  @IBOutlet private weak var singleImageView: UIImageView!
+}
