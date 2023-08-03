58d8e357ffe148099a491d58f5f291dce5eb125d
Sergey Kemenov <skemenov@icloud.com>
2023-08-03 09:11:17 +0300

    Added `Constants.swift` for Unsplash API

---
+++ ImageFeed-ios/API/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  ImageFeed-ios
+//
+//  Created by Sergey Kemenov on 03.08.2023.
+//
+
+import Foundation
+
+    // MARK: - Public properties
+
+// The API is to be used for non-automated, high-quality, and authentic experiences.
+// You cannot replicate the core user experience of Unsplash (unofficial clients, wallpaper applications, etc.).
+// Your Access Key and Secret Key must remain confidential.
+// Do not abuse the API. Too many requests too quickly will get your access turned off.
+
+public let accessKey = "IgH-MbheyUstaJWCnPaQc2gtrMXJyISlRHyzSdpZI5E"
+public let secreyKey = "H64ks3MlhldqtzwBGDXiLN6OdvL8ML0isXVDwI0hzb4"
+public let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+public let scopeAPI = "public+read_user"
+
