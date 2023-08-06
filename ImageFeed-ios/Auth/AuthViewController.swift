9d1da6c5287b7c173f9371d151cee66d9f82c3ba
Sergey Kemenov <skemenov@icloud.com>
2023-08-06 18:21:40 +0300

    Added `AuthViewController.swift` file into `Auth` folder

---
+++ ImageFeed-ios/Auth/AuthViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.swift
+//  ImageFeed-ios
+//
+//  Created by Sergey Kemenov on 06.08.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+  // MARK: - Public properties
+
+}
