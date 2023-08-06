451c959e850abaedf807a4a50bf28cd57110b8e7
Sergey Kemenov <skemenov@icloud.com>
2023-08-06 18:31:15 +0300

    Added `WebViewViewController.swift` file and `WebViewViewController` class

---
+++ ImageFeed-ios/Auth/WebViewViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed-ios
+//
+//  Created by Sergey Kemenov on 06.08.2023.
+//
+
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+  // MARK: - Private properties
+
+  private let showWebViewSegueIdentifier = "ShowWebView"
+
+}
+
+//extension WebViewViewController: WebViewViewController {
+//
+//}
