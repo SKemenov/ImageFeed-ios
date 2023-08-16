19d8472908ff2006b74335e32c1bd4e22a3afaa5
Sergey Kemenov <skemenov@icloud.com>
2023-08-16 11:07:08 +0300

    Fixed global constants in `Constants.swift`

--- ImageFeed-ios/API/Constants.swift
+++ ImageFeed-ios/API/Constants.swift
@@ -9,10 +9,11 @@ import Foundation
 
 // MARK: - Public properties
 
-// The API is to be used for non-automated, high-quality, and authentic experiences.
-// You cannot replicate the core user experience of Unsplash (unofficial clients, wallpaper applications, etc.).
-// Your Access Key and Secret Key must remain confidential.
-// Do not abuse the API. Too many requests too quickly will get your access turned off.
+// Unsplash API rules
+// 1. The API is to be used for non-automated, high-quality, and authentic experiences.
+// 2. You cannot replicate the core user experience of Unsplash (unofficial clients, wallpaper applications, etc.).
+// 3. Your Access Key and Secret Key must remain confidential.
+// 4. Do not abuse the API. Too many requests too quickly will get your access turned off.
 
 public let accessKey = "IgH-MbheyUstaJWCnPaQc2gtrMXJyISlRHyzSdpZI5E"
 public let secureKey = "H64ks3MlhldqtzwBGDXiLN6OdvL8ML0isXVDwI0hzb4"
@@ -20,5 +21,4 @@ public let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
 public let accessScope = "public+read_user+write_likes"
 
 // defaultBaseURL is optional
-public let defaultBaseURL = URL(string: "https://unsplash.com")
-public let defaultAPIURL = URL(string: "https://api.unsplash.com")
+public let defaultBaseURL = URL(string: "https://api.unsplash.com")
