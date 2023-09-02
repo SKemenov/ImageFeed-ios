f5fa3116d119ce9f33b1d371e6829280f132eeb2
Sergey Kemenov <skemenov@icloud.com>
2023-09-02 22:39:20 +0300

    Made `TabBatViewContriller` UI by code

--- ImageFeed-ios/Screens/TabBarView/TabBarController.swift
+++ ImageFeed-ios/Screens/TabBarView/TabBarController.swift
@@ -8,9 +8,30 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-  override func awakeFromNib() {
-    super.awakeFromNib()
+    setupTabBarViewController()
+  }
+}
+//  override func awakeFromNib() {
+//    super.awakeFromNib()
+//
+//    let storyboard = UIStoryboard(name: "Main", bundle: .main)
+//
+//    let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
+//
+//    let profileViewController = ProfileViewController()
+//    setupTabBarItem(for: profileViewController, image: "tab_profile_active")
+//
+//    self.viewControllers = [imagesListViewController, profileViewController]
+//  }
+// }
+
+private extension TabBarController {
+
+  func setupTabBarViewController() {
+    view.backgroundColor = .ypRed // .ypBackground
 
     let storyboard = UIStoryboard(name: "Main", bundle: .main)
 
@@ -19,16 +40,21 @@ final class TabBarController: UITabBarController {
     let profileViewController = ProfileViewController()
     setupTabBarItem(for: profileViewController, image: "tab_profile_active")
 
-    self.viewControllers = [imagesListViewController, profileViewController]
+    viewControllers = [imagesListViewController, profileViewController]
+    selectedIndex = 1
   }
-}
 
-private extension TabBarController {
-  func setupTabBarItem(for viewController: ProfileViewController, image: String) {
+  func setupTabBarItem(for viewController: UIViewController, image: String) {
     viewController.tabBarItem = UITabBarItem(
       title: nil,
       image: UIImage(named: image),
       selectedImage: nil
     )
+  }
+}
+
+extension TabBarController: UITabBarControllerDelegate {
+  func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+    selectedViewController = viewController
   }
 }
