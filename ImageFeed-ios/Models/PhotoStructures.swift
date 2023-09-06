88a7a151fd25fa2e3c07862b7e73036577df4179
Sergey Kemenov <skemenov@icloud.com>
2023-09-06 18:17:39 +0300

    Removed custom init and made it as a method in `ImageListService`

--- ImageFeed-ios/Models/PhotoStructures.swift
+++ ImageFeed-ios/Models/PhotoStructures.swift
@@ -12,16 +12,16 @@ import Foundation
 struct Photo {
   let id: String
   let size: CGSize
-  let createdAt: Date?
+  let createdAt: Date
   let welcomeDescription: String?
   let thumbImageURL: String
   let largeImageURL: String
   let isLiked: Bool
 }
 
-struct PhotoUrls: Codable {
+struct UrlsResult: Codable {
   let thumb: String
-  let regular: String
+  let full: String
 }
 
 // TODO: Use SnakeCaseJSONDecoder instead of SJONDecoder with CodingKeys enum
@@ -29,24 +29,8 @@ struct PhotoResult: Codable {
   let id: String
   let width: Int
   let height: Int
-  let createdAt: String?
+  let createdAt: String
   let description: String?
   let likedByUser: Bool
-  let urls: PhotoUrls
-}
-
-// MARK: - Init for Photo with PhotoResult
-
-extension Photo {
-  init(result photoResult: PhotoResult) {
-    self.init(
-      id: photoResult.id,
-      size: CGSize(width: Double(photoResult.width), height: Double(photoResult.height)),
-      createdAt: Date(),
-      welcomeDescription: photoResult.description,
-      thumbImageURL: photoResult.urls.thumb,
-      largeImageURL: photoResult.urls.regular,
-      isLiked: photoResult.likedByUser
-    )
-  }
+  let urls: UrlsResult
 }
