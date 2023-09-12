5ce66de4d887df94587faeaa16f7b5d7dc547939
Sergey Kemenov <skemenov@icloud.com>
2023-09-12 15:08:15 +0300

    Updated `ImageListService` methods

--- ImageFeed-ios/ServiceAccessLayer/ImageList/ImageListService.swift
+++ ImageFeed-ios/ServiceAccessLayer/ImageList/ImageListService.swift
@@ -26,13 +26,15 @@ final class ImageListService {
   private var currentTask: URLSessionTask?
 
   private var imagesPerPage = 10
-  private var lastLoadedPage: Int {
-    photos.count / imagesPerPage
-  }
+  private var lastLoadedPage: Int?
+//  private var lastLoadedPage: Int {
+//    photos.count / imagesPerPage
+//  }
 
   private (set) var photos: [Photo] = [] {
     didSet {
       NotificationCenter.default.post(name: ImageListService.didChangeNotification, object: self)
+      print("ITS LIT ILS 36 photos.count \(photos.count)")
     }
   }
 
@@ -46,7 +48,7 @@ private extension ImageListService {
   func makePhotoRequest(id: String) -> URLRequest? {
     let request = requestBuilder.makeHTTPRequest(path: "/photos/\(id)")
     // FIXME: Remove before PR Sprint 12
-    print("ITS LIT ILS 40 \(String(describing: request))")
+    print("ITS LIT ILS 49 \(String(describing: request))")
     return request
   }
 
@@ -54,13 +56,18 @@ private extension ImageListService {
     var request = requestBuilder.makeHTTPRequest(path: "/photos")
     request?.setValue("\(page)", forHTTPHeaderField: "page")
     request?.setValue("\(imagesPerPage)", forHTTPHeaderField: "per_page")
-    print("ITS LIT ILS 47 \(String(describing: request))")
+    print("ITS LIT ILS 57 request.header.page # \(String(describing: request?.value(forHTTPHeaderField: "page")))")
     return request
-//   Parameters
-//    param     Description
-//    page      Page number to retrieve. (Optional; default: 1)
-//    per_page  Number of items per page. (Optional; default: 10)
-//    order_by  How to sort the photos. Optional. (Valid values: latest, oldest, popular; default: latest)
+    //   Parameters
+    //    param     Description
+    //    page      Page number to retrieve. (Optional; default: 1)
+    //    per_page  Number of items per page. (Optional; default: 10)
+    //    order_by  How to sort the photos. Optional. (Valid values: latest, oldest, popular; default: latest)
+  }
+
+  func makeNextPageNumber() -> Int {
+    guard let lastLoadedPage else { return 1 }
+    return lastLoadedPage + 1
   }
 }
 
@@ -69,18 +76,19 @@ private extension ImageListService {
 extension ImageListService: ImageListLoading {
 
   func fetchPhotosNextPage() {
-    print("ITS LIT ILS 66 \(String(describing: currentTask))")
+//    print("ITS LIT ILS 72 \(String(describing: currentTask))")
 
     assert(Thread.isMainThread)
-    print("ITS LIT ILS 69 \(String(describing: lastLoadedPage))")
-    print("ITS LIT ILS 70 \(photos)")
+//    print("ITS LIT ILS 75 \(String(describing: lastLoadedPage))")
+//    print("ITS LIT ILS 76 photos.count \(photos.count)")
 
     guard currentTask == nil else {
-      print("ITS LIT ILS 73 reject repeated photos request")
+      print("ITS LIT ILS 79 Race Condition - reject repeated photos request")
       return
     }
 
-    let nextPage = lastLoadedPage + 1
+    let nextPage = makeNextPageNumber()
+    print("ITS LIT ILS 84 nextPage \(nextPage)")
 
     guard let request = makePhotosListRequest(page: nextPage) else {
       assertionFailure("Invalid request")
@@ -88,39 +96,43 @@ extension ImageListService: ImageListLoading {
       return
     }
 
-    print("ITS LIT ILS 81 \(request)")
+    print("ITS LIT ILS 92 \(request)")
 
     let task = session.objectTask(for: request) {
       [weak self] (result: Result<[PhotoResult], Error>) in
 
       guard let self else { preconditionFailure("Cannot make weak link") }
 
-      print("ITS LIT ILS 87 \(String(describing: self.currentTask))")
-      print("ITS LIT ILS 90 \(result)")
+//      print("ITS LIT ILS 99 \(String(describing: self.currentTask))")
+//      print("ITS LIT ILS 100 \(result)")
 
       switch result {
       case .success(let photoResults):
-        print("ITS LIT ILS 94 \(photoResults)")
+//        print("ITS LIT ILS 104 \(photoResults)")
 
         DispatchQueue.main.async {
           var photos: [Photo] = []
           photoResults.forEach { photo in
             photos.append(self.convert(result: photo))
+            print("ITS LIT ILS 116 append .photos.ID \(photo.id)")
           }
-          print("ITS LIT ILS 102 \(photos)")
+          print("ITS LIT ILS 111 photos.count \(photos.count)")
           self.photos += photos
-          print("ITS LIT ILS 104 \(self.photos)")
-          print("ITS LIT ILS 104 \(String(describing: self.lastLoadedPage))")
+          self.photos.forEach { photo in
+            print("ITS LIT ILS 121 self.photos.ID \(photo.id)")
+          }
+          self.lastLoadedPage = nextPage
+          print("ITS LIT ILS 114 self.lastLoadedPage \(String(describing: self.lastLoadedPage))")
         }
-        print("ITS LIT ILS 106 \(String(describing: lastLoadedPage))")
-        print("ITS LIT ILS 107 \(self.photos)")
-        print("ITS LIT ILS 107 done decode photos")
+        print("ITS LIT ILS 116 lastLoadedPage \(String(describing: lastLoadedPage))")
+        print("ITS LIT ILS 117 self.photos.count \(self.photos.count)")
+        print("ITS LIT ILS 118 done decode photos")
       case .failure(let error):
-        print("ITS LIT ILS 110 \(String(describing: error))")
+        print("ITS LIT ILS 120 \(String(describing: error))")
       }
       self.currentTask = nil
-      print("ITS LIT ILS 114 \(String(describing: self.currentTask))")
-      print("ITS LIT 115 \(self.photos)")
+      print("ITS LIT ILS 123 self.currentTask \(String(describing: self.currentTask))")
+      print("ITS LIT ILS 124 self.photos.count \(self.photos.count)")
     }
     self.currentTask = task
     task.resume()
@@ -130,16 +142,19 @@ extension ImageListService: ImageListLoading {
 // MARK: - Make Photo from PhotoResult's data
 
 private extension ImageListService {
-
   func convert(result photoResult: PhotoResult) -> Photo {
-    Photo(
+    let thumbWidth = 200.0
+    let aspectRatio = Double(photoResult.width) / Double(photoResult.height)
+    let thumbHeight = thumbWidth / aspectRatio
+    return Photo(
       id: photoResult.id,
       size: CGSize(width: Double(photoResult.width), height: Double(photoResult.height)),
-      createdAt: Constants.dateFormatter.date(from: photoResult.createdAt) ?? Date(), // FIXME: need to use formatter
+      createdAt: ISO8601DateFormatter().date(from: photoResult.createdAt ?? ""),
       welcomeDescription: photoResult.description,
       thumbImageURL: photoResult.urls.thumb,
       largeImageURL: photoResult.urls.full,
-      isLiked: photoResult.likedByUser
+      isLiked: photoResult.likedByUser,
+      thumbSize: CGSize(width: thumbWidth, height: thumbHeight)
     )
   }
 }
