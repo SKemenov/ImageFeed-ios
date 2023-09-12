5f8124ccabc8ea2822d1b06c94abf4f454468f4a
Sergey Kemenov <skemenov@icloud.com>
2023-09-12 16:39:01 +0300

    Removed old `config` method

--- ImageFeed-ios/Screens/TabBarView/ImageList/ImageListCell.swift
+++ ImageFeed-ios/Screens/TabBarView/ImageList/ImageListCell.swift
@@ -31,45 +31,29 @@ final class ImagesListCell: UITableViewCell {
 
 extension ImagesListCell {
 
-  func config(image: UIImage?, date: String, isLiked: Bool) {
-
-    let likedImage = isLiked ? UIImage(named: "like_active_on") : UIImage(named: "like_active_off")
-
-    imageCell.image = image
-    dateLabel.text = date
-    likeButton.setImage(likedImage, for: .normal)
-  }
-
   func loadCell(from photo: Photo) -> Bool {
     var status = false
 
     if let photoDate = photo.createdAt {
       dateLabel.text = Constants.dateFormatter.string(from: photoDate)
-    } else {
-      dateLabel.text = ""
     }
 
     let likedImage = photo.isLiked ? UIImage(named: "like_active_on") : UIImage(named: "like_active_off")
     likeButton.setImage(likedImage, for: .normal)
 
-
     guard let photoURL = URL(string: photo.thumbImageURL) else { return status }
 
-
     imageCell.kf.indicatorType = .activity
-    imageCell.kf.setImage(
-      with: photoURL,
-      placeholder: placeholderImage
-      ) { [weak self] result in
-        guard let self else { return }
-        switch result {
-        case .success(_):
-          status = true
-          print("ITS LIT ILC 66 Kingsfisher loaded an image")
-        case .failure(let error):
-          imageCell.image = placeholderImage
-          print("ITS LIT ILC 68 \(error.localizedDescription)")
-        }
+    imageCell.kf.setImage(with: photoURL, placeholder: placeholderImage) { [weak self] result in
+      guard let self else { return }
+      switch result {
+        // swiftlint:disable:next empty_enum_arguments
+      case .success(_):
+        status = true
+      case .failure(let error):
+        imageCell.image = placeholderImage
+        print("ITS LIT ILC 68 \(error.localizedDescription)")
+      }
     }
     return status
   }
