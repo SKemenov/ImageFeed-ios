70f554abd187cadb1dcb9715050ebc633496accd
Sergey Kemenov <skemenov@icloud.com>
2023-09-12 15:03:36 +0300

    Added `loadCell` method for loading image from Unsplash API

--- ImageFeed-ios/Screens/TabBarView/ImageList/ImageListCell.swift
+++ ImageFeed-ios/Screens/TabBarView/ImageList/ImageListCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class ImagesListCell: UITableViewCell {
   // MARK: - Outlets
@@ -14,9 +15,16 @@ final class ImagesListCell: UITableViewCell {
   @IBOutlet private weak var dateLabel: UILabel!
   @IBOutlet private weak var likeButton: UIButton!
 
+  private let placeholderImage = UIImage(named: "stub")
+
   // MARK: - Public properties
 
   static let reuseIdentifier = "ImagesListCell"
+
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    imageCell.kf.cancelDownloadTask()
+  }
 }
 
 // MARK: - Methods
@@ -30,5 +38,39 @@ extension ImagesListCell {
     imageCell.image = image
     dateLabel.text = date
     likeButton.setImage(likedImage, for: .normal)
+  }
+
+  func loadCell(from photo: Photo) -> Bool {
+    var status = false
+
+    if let photoDate = photo.createdAt {
+      dateLabel.text = Constants.dateFormatter.string(from: photoDate)
+    } else {
+      dateLabel.text = ""
+    }
+
+    let likedImage = photo.isLiked ? UIImage(named: "like_active_on") : UIImage(named: "like_active_off")
+    likeButton.setImage(likedImage, for: .normal)
+
+
+    guard let photoURL = URL(string: photo.thumbImageURL) else { return status }
+
+
+    imageCell.kf.indicatorType = .activity
+    imageCell.kf.setImage(
+      with: photoURL,
+      placeholder: placeholderImage
+      ) { [weak self] result in
+        guard let self else { return }
+        switch result {
+        case .success(_):
+          status = true
+          print("ITS LIT ILC 66 Kingsfisher loaded an image")
+        case .failure(let error):
+          imageCell.image = placeholderImage
+          print("ITS LIT ILC 68 \(error.localizedDescription)")
+        }
+    }
+    return status
   }
 }
