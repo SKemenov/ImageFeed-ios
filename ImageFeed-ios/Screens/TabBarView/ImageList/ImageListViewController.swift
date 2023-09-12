801a595d3b2a77dc66ed619d13eaf8a30217b6dc
Sergey Kemenov <skemenov@icloud.com>
2023-09-12 16:40:09 +0300

    Removed temp prints, refactor the code

--- ImageFeed-ios/Screens/TabBarView/ImageList/ImageListViewController.swift
+++ ImageFeed-ios/Screens/TabBarView/ImageList/ImageListViewController.swift
@@ -30,7 +30,6 @@ final class ImagesListViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    alertPresenter = AlertPresenter(viewController: self)
     setupTableView()
     setupImageListService()
     setupNotificationObserver()
@@ -59,7 +58,6 @@ final class ImagesListViewController: UIViewController {
 private extension ImagesListViewController {
 
   func setupTableView() {
-
     tableView.dataSource = self
     tableView.delegate = self
     tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 0, right: 0)
@@ -82,11 +80,9 @@ private extension ImagesListViewController {
   }
 
   func updateTableViewAnimated() {
-    print("ITS LIT ILVC 89 run updateTableViewAnimated()")
     let oldCount = photos.count
     photos = imageListService.photos
     let newCount = photos.count
-    print("ITS LIT ILVC 93 oldCount \(oldCount) newCount \(newCount)")
 
     if oldCount != newCount {
       tableView.performBatchUpdates {
@@ -114,8 +110,7 @@ extension ImagesListViewController: UITableViewDelegate {
 extension ImagesListViewController: UITableViewDataSource {
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    print("ITS LIT ILVC 139 \(photos.count)")
-    return photos.count
+    photos.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -126,10 +121,7 @@ extension ImagesListViewController: UITableViewDataSource {
       return UITableViewCell()
     }
 
-    let photo = photos[indexPath.row]
-//    print("ITS LIT ILVC 152 indexPath.row \(indexPath.row) \(photo)")
-    let isLoadedCell = cell.loadCell(from: photo)
-    if isLoadedCell {
+    if cell.loadCell(from: photos[indexPath.row]) {
       tableView.reloadRows(at: [indexPath], with: .automatic)
     }
     return cell
@@ -142,13 +134,11 @@ extension ImagesListViewController: UITableViewDataSource {
     let imageWidth = thumbImageSize.width
     let scale = imageViewWidth / imageWidth
     let cellHeight = thumbImageSize.height * scale + imageInsets.top + imageInsets.bottom
-//    print("ITS LIT ILVC 187 scale \(scale) cell #\(indexPath.row) Height \(cellHeight)")
     return cellHeight
   }
 
   func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
     if indexPath.row + 1 == photos.count {
-      print("ITS LIT ILVC 193 run loading next page of images, because indexPath.row \(indexPath.row)")
       imageListService.fetchPhotosNextPage()
     }
   }
