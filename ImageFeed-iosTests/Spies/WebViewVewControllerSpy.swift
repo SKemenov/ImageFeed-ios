73aef257fa2722db42c7beba9fb74ffab8e8c98a
Sergey Kemenov <skemenov@icloud.com>
2023-09-22 08:57:18 +0300

    Made `WebViewVewControllerSpy` class

---
+++ ImageFeed-iosTests/Spies/WebViewVewControllerSpy.swift
@@ -0,0 +1,22 @@
+//
+//  WebViewVewControllerSpy.swift
+//  ImageFeed-iosTests
+//
+//  Created by Sergey Kemenov on 22.09.2023.
+//
+
+import Foundation
+import ImageFeed_ios
+
+final class WebViewVewControllerSpy: WebViewViewControllerProtocol {
+  var presenter: ImageFeed_ios.WebViewPresenterProtocol?
+  var viewDidLoadRequest = false
+
+  func load(request: URLRequest) {
+    viewDidLoadRequest = true
+  }
+
+  func setProgressValue(_ newValue: Float) { }
+
+  func setProgressHidden(_ flag: Bool) { }
+}
