5fcc5b031a48e5ec929ebba094366de452cc5497
Sergey Kemenov <skemenov@icloud.com>
2023-09-23 17:41:25 +0300

    Removed unnecessarily default methods from `WebViewTests`

--- ImageFeed-iosTests/UnitTests/WebViewTests.swift
+++ ImageFeed-iosTests/UnitTests/WebViewTests.swift
@@ -12,14 +12,6 @@ import XCTest
 
 final class WebViewTests: XCTestCase {
 
-  override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-  }
-
-  override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-  }
-
   func testViewControllerCallsViewDidLoad() {
     // given
     let storyboard = UIStoryboard(name: "Main", bundle: nil)
